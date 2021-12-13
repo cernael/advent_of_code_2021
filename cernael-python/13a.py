ccf8d5d1b45f49e07dfe9123416b0a616e4098b0
Tomas Kronvall <tomas@cernael.com>
2021-12-13 13:58:14 +0100

    cernael day 13

---
+++ cernael-python/13a.py
@@ -0,0 +1,42 @@
+def solve(lines):
+    dots = set()
+    folds = []
+    t1 = True
+    for l in lines:
+        if not l:
+            t1 = False
+            continue
+        if t1:
+            x, y = l.split(',')
+            dots.add((int(x),int(y)))
+        else:
+            r = l.split()[2].split('=')
+            folds.append([r[0],int(r[1])])
+
+    dir,line = folds[0]
+
+    if dir == 'x':
+        adds, removes = set(), set()
+        for d in dots:
+            if d[0] > line:
+                removes.add(d)
+                adds.add((line*2 - d[0], d[1]))
+        dots |= adds
+        dots -= removes
+    elif dir == 'y':
+        adds, removes = set(), set()
+        for d in dots:
+            if d[1] > line:
+                removes.add(d)
+                adds.add((d[0], line*2 - d[1]))
+        dots |= adds
+        dots -= removes
+
+    return len(dots)
+
+if __name__ == '__main__':
+    lines = []
+    with open('13.txt') as f:
+        for line in f.readlines():
+            lines.append(line.strip())
+    print(solve(lines))
---
+++ cernael-python/13b.py
@@ -0,0 +1,44 @@
+def solve(lines):
+    dots = set()
+    folds = []
+    t1 = True
+    for l in lines:
+        if not l:
+            t1 = False
+            continue
+        if t1:
+            x, y = l.split(',')
+            dots.add((int(x),int(y)))
+        else:
+            r = l.split()[2].split('=')
+            folds.append([r[0],int(r[1])])
+
+    for f in folds:
+        dir,line = f
+
+        if dir == 'x':
+            adds, removes = set(), set()
+            for d in dots:
+                if d[0] > line:
+                    removes.add(d)
+                    adds.add((line*2 - d[0], d[1]))
+            dots |= adds
+            dots -= removes
+        elif dir == 'y':
+            adds, removes = set(), set()
+            for d in dots:
+                if d[1] > line:
+                    removes.add(d)
+                    adds.add((d[0], line*2 - d[1]))
+            dots |= adds
+            dots -= removes
+
+    map = [['#' if (x,y) in dots else '.' for x in range(40)] for y in range(7)]
+    for l in map: print(''.join(l))
+
+if __name__ == '__main__':
+    lines = []
+    with open('13.txt') as f:
+        for line in f.readlines():
+            lines.append(line.strip())
+    print(solve(lines))
